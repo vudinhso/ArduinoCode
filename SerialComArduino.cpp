3f79426d8ae828275ccc62e16261dbd981ec1a81
Dinh <81209378+vudinhso@users.noreply.github.com>
2023-06-15 16:01:03 +0700

    Update SerialComArduino.cpp
    Update the code with comments.

--- SerialComArduino.cpp
+++ SerialComArduino.cpp
@@ -1,24 +1,36 @@
+// This code is for the Arduino board. 
+// First, it states the elapsed time. 
+// Then, it states the potentiometer angle. 
+// The elapsed time and the potentiometer angle are separated with a comma. 
+// Serial.write(13);  Serial.write(10); --> Correspond to a line break (CR/LF: CR = Carriage Return and LF = Line Feed), 
+// which is the delimiter, so that MATLAB knows where the data from the serial communication ends and starts. 
+
+// Custom map function
 float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+// Pin declaration
+int PotPin = A0;
+
 void setup() {
   Serial.begin(9600);
   delay(100);
 }
 
-float angle;
+// Variable declaration
+int PotVal; 
+float PotAngle;
 unsigned long myTime;
 
 void loop() {
-  int sensorValue = analogRead(A0); //read the input on analog pin 0
+  PotVal = analogRead(PotPin); 
+  PotAngle = floatMap(PotVal,0,1023,0,300);
   myTime = millis();
-  angle = floatMap(sensorValue,0,1023,0,270);
 
-
-  //Serial.print("angle:");
   Serial.print(myTime);
   Serial.print(",");
-  Serial.print(angle);
+  Serial.print(PotAngle);
   Serial.write(13);
   Serial.write(10);
   delay(10); 
