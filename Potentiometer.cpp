75f0df629c21c7e767de5aa3110a1e1aa370315a
Dinh <81209378+vudinhso@users.noreply.github.com>
2023-07-12 16:25:00 +0700

    Update Potentiometer.cpp

--- Potentiometer.cpp
+++ Potentiometer.cpp
@@ -1,25 +1,47 @@
 #include <Arduino.h>
 
-// floatMap is like the built-in map function, but output a float
-float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-int sensorPin = A2;
-
-void setup() {
+// ******************************************
+// Variable declaration
+// ******************************************
+int PotPin = A0;
+unsigned long curr_time = 0, prev_time = 0, dt = 50000; // time interval in us
+// ******************************************
+// Function declaration
+// ******************************************
+float floatMap(float, float, float, float, float); // like the map function, but with float instead of integer
+// ******************************************
+// void setup
+// ******************************************
+void setup()
+{
   Serial.begin(9600);
+  analogReadResolution(12); // Only for microctrl with 12bit ADC
 }
-float angle0;
-void loop() {
-  int sensorValue0 = analogRead(sensorPin);
-  angle0 = floatMap(sensorValue0,0,1023,0,300);
-  // print out the value you read:
-  Serial.print("bitvalue: ");
-  Serial.print(sensorValue0);
-  Serial.print("--- angle: ");
-  Serial.print(angle0);
-  Serial.print(" deg.");
-  Serial.println("");
-  delay(100);        // delay in between reads for stability
+// ******************************************
+// void loop
+// ******************************************
+void loop()
+{
+  // Data acquisition
+  int PotVal = analogRead(PotPin);
+  float Angle = floatMap(PotVal, 0, 4096, 0, 300);
+  curr_time = micros();
+  // Write on the COM each dt interval
+  if (curr_time - prev_time >= dt)
+  {
+    prev_time += dt;
+    Serial.print(curr_time / 1000);
+    Serial.print(",");
+    Serial.print(PotVal);
+    Serial.print(",");
+    Serial.print(Angle);
+    Serial.println("");
+  }
+}
+// ******************************************
+// Function definition
+// ******************************************
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
