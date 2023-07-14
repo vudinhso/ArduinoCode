608656511fbaf63c901d014eee67d807eca4e51d
Dinh <81209378+vudinhso@users.noreply.github.com>
2023-07-14 12:32:29 +0700

    Update Loadcell.cpp

--- Loadcell.cpp
+++ Loadcell.cpp
@@ -16,7 +16,7 @@
 HX711_ADC scale(LOADCELL_DT_PIN, LOADCELL_SCK_PIN);
 #define calibration_factor 460 // Depends on the load cell
 volatile boolean newDataReady;
-float loadcell_data;           // measurement from the loadcell in gr
+float loadcell_data; // measurement from the loadcell in gr
 // Serial configuration parameters
 unsigned long curr_time = 0, prev_time = 0, dt = 50000; // time interval in us
 
@@ -57,11 +57,12 @@ void loop()
     loadcell_data = scale.getData();
   }
 
+  curr_time = micros();
   if (curr_time - prev_time >= dt)
   {
+    prev_time += dt;
     SerialDataWrite();
   }
-  
 }
 // ******************************************
 // Function definitions
@@ -69,15 +70,13 @@ void loop()
 void dataReadyISR()
 {
   if (scale.update())
-  {
     newDataReady = 1;
-  }
 }
 
 void SerialDataWrite()
 {
-    Serial.print(curr_time / 1000);
-    Serial.print(",");
-    Serial.print(loadcell_data);
-    Serial.println("");
+  Serial.print(curr_time / 1000);
+  Serial.print(",");
+  Serial.print(loadcell_data);
+  Serial.println("");
 }
