33c496235ad9c919c338cb666131914e413b0b59
Dinh <81209378+vudinhso@users.noreply.github.com>
2023-07-15 14:10:48 +0700

    Update GPS_Adafruit_Teensy.cpp

--- GPS_Adafruit_Teensy.cpp
+++ GPS_Adafruit_Teensy.cpp
@@ -3,29 +3,29 @@
 #include <SPI.h>
 #include <Wire.h>
 
-// #include <SoftwareSerial.h>
-// #include <Adafruit_GPS.h>
-
+//**********************************************
+// Variable declaration
+//**********************************************
 const int chipSelect = BUILTIN_SDCARD;
+String inputSerial1 = "";       // a string to hold incoming data
+boolean IsReadySerial1 = false; // whether the string is complete
+File mySensorData;              // Data object you will write your sesnor data to
 
-// put function declarations here:
+//**********************************************
+// Function declaration
+//**********************************************
 void SelectSentences();
 void serialEvent1();
 
-String inputSerial1 = "";       // a string to hold incoming data
-boolean IsReadySerial1 = false; // whether the string is complete
-
-// String NMEA1; // We will use this variable to hold our first NMEA sentence
-// char c;       // Used to read the characters spewing from the GPS module
-
-File mySensorData; // Data object you will write your sesnor data to
-
+//**********************************************
+// Setup
+//**********************************************
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial)
     ; // wait for serial port to connect.
+  // GPS initialization
   Serial.println("Initializing GPS ...");
   Serial1.begin(9600);
   delay(100);
@@ -33,8 +33,8 @@ void setup()
   delay(100);
   Serial.println("GPS Initialized");
 
+  // SD initialization
   Serial.println("Initializing SD card...");
-
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect))
   {
@@ -53,26 +53,25 @@ void setup()
   delay(100);
 }
 
+//**********************************************
+// Main loop
+//**********************************************
 void loop()
 {
-  // Does Seria11 have a new message?
-  if (IsReadySerial1)
+  if (IsReadySerial1) // Does Seria11 have a new message?
   {
-    // Print new message on a new line.
-    // The last character of the inputSerial1 is a new line
-    Serial.print(inputSerial1);
     mySensorData = SD.open("NMEA.txt", FILE_WRITE); // Open file on SD card for writing
     mySensorData.println(inputSerial1);             // Write first NMEA to SD card
     mySensorData.close();                           // Close the file
-    // Serial.println("data logged");
-
-    // clear the string:
-    inputSerial1 = "";
-    IsReadySerial1 = false;
+    Serial.print(inputSerial1);                     // Write on the serial monitor for debugging.
+    inputSerial1 = "";                              // Clear the string:
+    IsReadySerial1 = false;                         // Reset the serial flag
   }
 }
 
-// put function definitions here:
+//**********************************************
+// Function definition
+//**********************************************
 void SelectSentences()
 {
   // Select RMC and GGA sentences
@@ -87,12 +86,8 @@ void serialEvent1()
 
   while (Serial1.available() && IsReadySerial1 == false)
   {
-    // Read the new byte:
-    char nextChar = (char)Serial1.read();
-
-    // Append to the inputSerial1 string
-    inputSerial1 += nextChar;
-
+    char nextChar = (char)Serial1.read(); // Read the new byte:
+    inputSerial1 += nextChar;             // Append to the inputSerial1 string
     // If a newline, then set flag so that the main loop will process the string
     if (nextChar == '\n')
     {
