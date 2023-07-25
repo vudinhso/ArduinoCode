40322a0bb71beb4bcac92bfd330e4c09b9e95f80
Dinh <81209378+vudinhso@users.noreply.github.com>
2023-07-25 07:54:04 +0700

    Update 02_Potentiometer.cpp

--- 02_Potentiometer.cpp
+++ 02_Potentiometer.cpp
@@ -9,6 +9,8 @@ unsigned long curr_time = 0, prev_time = 0, dt = 50000; // time interval in us
 // Function declaration
 // ******************************************
 float floatMap(float, float, float, float, float); // like the map function, but with float instead of integer
+void SerialDataWrite();
+
 // ******************************************
 // void setup
 // ******************************************
@@ -30,12 +32,7 @@ void loop()
   if (curr_time - prev_time >= dt)
   {
     prev_time += dt;
-    Serial.print(curr_time / 1000);
-    Serial.print(",");
-    Serial.print(PotVal);
-    Serial.print(",");
-    Serial.print(Angle);
-    Serial.println("");
+    SerialDataWrite();
   }
 }
 // ******************************************
@@ -44,4 +41,14 @@ void loop()
 float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void SerialDataWrite()
+{
+  Serial.print(curr_time / 1000);
+  Serial.print(",");
+  Serial.print(PotVal);
+  Serial.print(",");
+  Serial.print(Angle);
+  Serial.println("");
 }
